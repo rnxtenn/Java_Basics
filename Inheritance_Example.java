bcb90156617c680a6afc98d984fb7f90c405d0f6
Rahul Sarkar <rnxart5798@gmail.com>
2025-05-09 01:42:29 +0530

    adding super

--- Inheritance_Example.java
+++ Inheritance_Example.java
@@ -1,19 +1,26 @@
 package Java_Basics;
 
 class parent{
-    public parent(){
+    public parent(){ // non parameterised Constructor
         System.out.println("Parent class Called");
+    }
+    public parent(int y){
+        System.out.println("parent class Parameterised Constructor");
     }
 }
 class child extends parent{
     public child(){
         System.out.println("child class Called");
     }
+    public child(int x){
+        super(4);
+        System.out.println("child class parameterised constructor");
+    }
 }
 
 
 public class Inheritance_Example {
     public static void main(String[] args){
-        child c = new child();
+        child c = new child(7);
     }
 }
